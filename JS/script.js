82f32e9ff14f23d17353aed268c6c437f866dc5a
akulshina.o.a@gmail.com <akulshina.o.a@gmail.com>
2023-10-15 16:16:58 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+//let number = 5;
+//const leftBorderWidth = 1;
+
+//number = 10;
+//console.log(number);
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+const answer = prompt('Вам есть 18?', '18');
+console.log(typeof(answer));
